fb2b0cf0412b1b058422bc6ababe64fb7c6cf07b
Bello Opeoluwa <98402971+opeoluwa44@users.noreply.github.com>
2022-04-28 15:31:49 +0100

    Srefix

--- Google clone/google.js
+++ Google clone/google.js
@@ -6,18 +6,19 @@ const nav = document.querySelector(".apps")
 
 
 function search(){
+    if(e.key==="Enter"){
     const input = searchInput.value
     searchBtn.href = `https://www.google.com/search?q=${input}&sxsrf=APq-WBsWBEKNTNxEh9bf5Ym22sM-yuljow%3A1650364765806&source=hp&ei=XZFeYvDBL9HSa-f3vMgN&iflsig=AHkkrS4AAAAAYl6fbTRSEvP5F2VFWH7sNPIK4RTq6cRX&ved=0ahUKEwiw5PTs95_3AhVR6RoKHec7D9kQ4dUDCAc&uact=5&oq=${input}&gs_lcp=Cgdnd3Mtd2l6EAMyBAgjECcyBAgjECcyBAgjECcyCAgAEIAEELEDMgUIABCABDIFCAAQgAQyCwgAELEDEIMBEIsDMgsIABCxAxCDARCLAzIFCAAQgAQyCAgAELEDEIMBOgcIIxDqAhAnOggILhDUAhCRAjoFCC4QkQI6BQgAELEDOgUILhCxAzoECAAQA1DOCFitEWCeFmgBcAB4AIABiAKIAYIGkgEDMi0zmAEAoAEBsAEKuAEB&sclient=gws-wiz`
-
+   }
 }
 
 searchBtn.addEventListener("click", search)
 
-document.body.addEventListener("keydown", (e)=>{
-    if(e.key === 'Enter'){
-        search()
-    }
-})
+searchInput.addEventListener("keydown", search)
+
+    
+  
+
 
 
 searchIcon.addEventListener("click", ()=>{
